e14d6c4252fc3377dc6b46e2831e8469dd8d6a3f
jitammy <jitammy@gmail.com>
2019-07-09 21:25:12 -0400

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 
+
+
 module.exports = {
 
   development: {
+
     client: 'postgresql',
     connection: {
       host     : process.env.DB_HOST,
       user     : process.env.DB_USER,
       password : process.env.DB_PASS,
       database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      port     : process.env.DB_PORT
     },
     migrations: {
       directory: './db/migrations',
@@ -22,14 +24,18 @@ module.exports = {
   },
 
   production: {
+
     client: 'postgresql',
     connection: process.env.DATABASE_URL + '?ssl=true',
+
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+
       tableName: 'migrations'
+
     }
   }
 
